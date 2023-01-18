f01224c0efc2b0caae91a6f149db27d41f6cfdae
Kolten Brosseau <koltenjb@gmail.com>
2023-01-18 18:37:30 -0500

    Added toString method

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -18,4 +18,15 @@ export class ContentList {
   getLength() {
     return this._chess.length;
   }
+
+  toString(num: number) {
+    let indexedItem: IContent = this._chess[num];
+    let tags = indexedItem.tags!;
+
+    let stringToOutput = "<h1>".concat(indexedItem.title, "</h1><br>", "<h2>Description: ", indexedItem.description!, "</h2><br><h2>Author: ", indexedItem.author, "</h2><br><img src='", indexedItem.imgSrc!, "'/><br>", "<h2>Type: ", indexedItem.type, "</h2><br><h2>Tags: </h2><br>");
+
+    for (let i = 0; i < tags?.length; i++) {
+      stringToOutput.concat("<ul>", tags[i], "</ul>");
+    }
+  }
 }
