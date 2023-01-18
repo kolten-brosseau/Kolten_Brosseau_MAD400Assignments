78a9100b1b838ff8c0637c57b8bf146d4bf4e404
Kolten Brosseau <koltenjb@gmail.com>
2023-01-18 18:12:51 -0500

    Added methods to content class

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -1,2 +1,21 @@
+import { IContent } from "./icontent";
+
 export class ContentList {
+  private _chess: IContent[];
+
+  constructor(chess: IContent) {
+    this._chess = [];
+  }
+
+  getChess() {
+    return this._chess;
+  }
+
+  addChess(chessToAdd: IContent) {
+    this._chess.push(chessToAdd);
+  }
+
+  getLength() {
+    return this._chess.length;
+  }
 }
