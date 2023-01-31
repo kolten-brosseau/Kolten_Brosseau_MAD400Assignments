16abc472f012eb95da09f4c3963ce1e18dfd9694
Kolten Brosseau <koltenjb@gmail.com>
2023-01-30 22:16:25 -0500

    Added Array Populated array

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { IContent } from '../models/icontent';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
-export class ContentListComponent {
 
+
+
+export class ContentListComponent {
+  playersArray: IContent[] = [{
+    id: 0,
+    title: "Magnus Carlsen",
+    author: "Kolten Brosseau",
+    type: "Person"
+  },
+    {
+      id: 1,
+      title: "Anish Giri",
+      author: "Kolten Brosseau",
+      type: "Person"
+    },
+    {
+      id: 2,
+      title: "Hikaru Nakumara",
+      author: "Kolten Brosseau",
+      type: "Person"
+    },
+    {
+      id: 3,
+      title: "Gary Kasparov",
+      author: "Kolten Brosseau",
+      type: "Person"
+    }  ]
 }
