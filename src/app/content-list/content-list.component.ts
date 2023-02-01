2950538c724d7bbfa22c8c766d88708bfd68691d
Kolten Brosseau <koltenjb@gmail.com>
2023-02-01 18:58:46 -0500

    Began Assignment 3

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -8,33 +8,6 @@ import { IContent } from '../models/icontent';
 })
 export class ContentListComponent {
 
-  playersArray: IContent[];
-
-  constructor() {
-    this.playersArray = [{
-      id: 0,
-      title: "Magnus Carlsen",
-      author: "Kolten Brosseau",
-      type: "Person"
-    },
-    {
-      id: 1,
-      title: "Anish Giri",
-      author: "Kolten Brosseau",
-      type: "Person"
-    },
-    {
-      id: 2,
-      title: "Hikaru Nakumara",
-      author: "Kolten Brosseau",
-      type: "Person"
-    },
-    {
-      id: 3,
-      title: "Gary Kasparov",
-      author: "Kolten Brosseau",
-      type: "Person"
-    }];
-  }
+  
 
 }
---
+++ src/app/data/mock-content.ts
@@ -0,0 +1,29 @@
+
+import { Component } from '@angular/core';
+import { IContent } from '../models/icontent';
+
+var playersArray: IContent[] = [{
+      id: 0,
+      title: "Magnus Carlsen",
+      author: "Kolten Brosseau",
+      type: "Person"
+    },
+    {
+      id: 1,
+      title: "Anish Giri",
+      author: "Kolten Brosseau",
+      type: "Person"
+    },
+    {
+      id: 2,
+      title: "Hikaru Nakumara",
+      author: "Kolten Brosseau",
+      type: "Person"
+    },
+    {
+      id: 3,
+      title: "Gary Kasparov",
+      author: "Kolten Brosseau",
+      type: "Person"
+    }];
+  }
