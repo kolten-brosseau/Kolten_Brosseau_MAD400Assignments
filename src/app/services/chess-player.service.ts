b46bbefd389971cea53096b26194c7f4b567dcec
Kolten Brosseau <koltenjb@gmail.com>
2023-02-03 09:42:30 -0500

    Added method to return observable array

--- src/app/services/chess-player.service.ts
+++ src/app/services/chess-player.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { playersArray } from '../data/mock-content';
+import { IContent } from '../models/icontent';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,9 @@ import { Injectable } from '@angular/core';
 export class ChessPlayerService {
 
   constructor() { }
+
+getContent(): Observable<IContent[]>{
+  return of(playersArray);
+}
+
 }
